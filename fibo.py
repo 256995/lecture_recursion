409b43b2cee96731d35b9480815bdacac7c64ecd
256995 <256995@vutbr.cz>
2024-04-24 09:33:20 +0200

    fibonacci

--- fibo.py
+++ fibo.py
@@ -1,6 +1,19 @@
+def recursive_nth_fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
 
 def main():
-    pass
+    index = input('Kolik clenu Fibonacciho posloupnosti chcete zobrazit? ')
+    index = int(index)
+    seq = []
+    for i in range(index):
+        seq.append(recursive_nth_fibo(i))
+    # jednoradkova syntax:
+    # seq = [recursive_nth_fibo(i) for i in range(index)]
+    print(seq)
 
 
 if __name__ == "__main__":
